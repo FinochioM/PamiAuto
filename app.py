d3551c97ac280273874769443532e0126d974e21
maglopeziacoi <maglopeziacoi@gmail.com>
2025-08-10 12:39:57 -0300

    eliminar funcion

--- app.py
+++ app.py
@@ -11,14 +11,6 @@ from logs_window import LogsWindow
 from settings_manager import SettingsManager
 from settings_window import SettingsWindow
 
-def resource_path(relative_path):
-    """Get absolute path to resource, works for dev and for PyInstaller"""
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
-
 class WorkerSignals(QObject):
     status_update = pyqtSignal(str, str)
     error_occurred = pyqtSignal(str)
